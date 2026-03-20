75a5f82b9028ede21d84927acd3b3780f66e10c7
Ccx-gs <2737106950@qq.com>
2026-03-20 19:16:25 +0800

    订单定时任务类功能代码

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -7,6 +7,9 @@ import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
 
@@ -43,4 +46,13 @@ void insert(Orders orders);
      * @param ordersPageQueryDTO
      */
     Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    /**
+     *根据订单状态和下单时间查询订单
+      * @param status
+     * @param orderTime
+     * @return
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+        List<Orders>getByStatusAndOrderTime(Integer status, LocalDateTime orderTime);
 }
